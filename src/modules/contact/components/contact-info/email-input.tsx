3fe1b832d5efa070d43acbb58343a2f32ed529f1
pedro-novo <pedro.novo.93@gmail.com>
2023-09-04 19:14:40 +0100

    add inputs border color

--- src/modules/contact/components/contact-info/email-input.tsx
+++ src/modules/contact/components/contact-info/email-input.tsx
@@ -16,7 +16,7 @@ const EmailInput: React.FC<Props> = ({ value, onChange }) => {
           type='email'
           name='email'
           id='email'
-          className='block w-full rounded-md border-1 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+          className='block w-full rounded-md border-1 border-gray-300 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
           placeholder='you@example.com'
           value={value}
           onChange={(e) => onChange(e.target.value)}
