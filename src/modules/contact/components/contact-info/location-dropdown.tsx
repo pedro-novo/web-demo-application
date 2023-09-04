718acb5dfa72989ac71c1d8a4d20b29450e2caad
pedro-novo <pedro.novo.93@gmail.com>
2023-09-04 18:53:43 +0100

    update borders

--- src/modules/contact/components/contact-info/location-dropdown.tsx
+++ src/modules/contact/components/contact-info/location-dropdown.tsx
@@ -14,7 +14,7 @@ const LocationDropdown: React.FC<Props> = ({ value, onChange }) => {
       <select
         id='location'
         name='location'
-        className='mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6'
+        className='mt-2 block w-full rounded-md border-1 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6'
         value={value}
         onChange={(e) => onChange(e.target.value)}
       >
